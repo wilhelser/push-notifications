e0b0f69719d5a656a92d2b4e89fa33f6fbb14c6d
Wil Helser <wil@wilh.me>
2023-11-03 09:04:30 -0500

    updates here

--- app/javascript/controllers/push_controller.js
+++ app/javascript/controllers/push_controller.js
@@ -30,7 +30,7 @@ export default class extends Controller {
                 serviceWorkerRegistration.pushManager
                   .subscribe({
                     userVisibleOnly: true,
-                    applicationServerKey: window.vapidPublicKey,
+                    applicationServerKey: vapidPublicKey,
                   })
                   .then((subscription) => {
                     this.saveSubscription(subscription);
